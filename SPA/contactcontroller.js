c0e464ed5cd80c713bdc17b2137330e29f2748ad
Neeraj121296 <neeraj121296@gmail.com>
2017-12-05 22:44:12 +0530

    Update contactcontroller.js

--- SPA/contactcontroller.js
+++ SPA/contactcontroller.js
@@ -1,4 +1,4 @@
 app.controller("contactctrl",function($scope,$routeParams){
 $scope.contact="delhi-7";
-    $scope.name = "Brain Mentors "+$routeParams.firstname+" "+$routeParams.lastname;
+    $scope.name = "KDP "+$routeParams.firstname+" "+$routeParams.lastname;
 })
